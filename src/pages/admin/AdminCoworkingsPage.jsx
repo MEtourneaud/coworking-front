b72734a9474ae72e50ac4c87e20ba432f1c814f1
Mathieu Etourneaud <mathieu.etourneaud@lapiscine.pro>
2023-12-20 11:40:42 +0100

    Add decodedToken.data.role in AdminCoworkingsPage

--- src/pages/admin/AdminCoworkingsPage.jsx
+++ src/pages/admin/AdminCoworkingsPage.jsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react"
 import HeaderAdmin from "../../components/admin/HeaderAdmin"
 import { Link } from "react-router-dom"
+import { jwtDecode } from "jwt-decode"
 
 const AdminCoworkingsPage = () => {
   const [coworkings, setCoworkings] = useState(null)
+  //Je récupère le token depuis le localStorage
+  const token = localStorage.getItem("jwt")
+  //Je décode le token
+  const decodedToken = jwtDecode(token)
 
   useEffect(() => {
     ;(async () => {
@@ -14,9 +19,6 @@ const AdminCoworkingsPage = () => {
   }, [])
 
   const handleDeleteCoworking = async (event, coworkingId) => {
-    //Je récupère le token depuis le localStorage
-    const token = localStorage.getItem("jwt")
-
     //J'effectue une requête FETCH de type DELETE pour supprimer le coworking avec l'ID  spécifié
     await fetch(`http://localhost:3000/api/coworkings/${coworkingId}`, {
       method: "DELETE",
@@ -42,9 +44,13 @@ const AdminCoworkingsPage = () => {
             return (
               <article>
                 <h3>{coworking.name}</h3>
-                <button onClick={(event) => handleDeleteCoworking(event, coworking.id)}>
-                  Supprimer
-                </button>
+                {/* Si decodedToken.data.role est différent de 3, alors j'affiche le bouton "supprimer".
+                Si decodedToken.data.role est égale à 3, alors le bouton sera masqué */}
+                {decodedToken.data.role !== 3 && (
+                  <button onClick={(event) => handleDeleteCoworking(event, coworking.id)}>
+                    Supprimer
+                  </button>
+                )}
                 <Link to={`/admin/coworkings/update/${coworking.id}`}>
                   <button>Modifier</button>
                 </Link>
